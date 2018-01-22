a6f0675e0ad829efdd022b89b8e8a7cc9c74f86f
julientregoat <julienctregoat@gmail.com>
2018-01-22 13:58:04 -0500

    can only access login page if logged out, some minor layout changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :logged_in?
+  skip_before_action :logged_in?, only: :index
 
   def index
   end
 
   def current_user
-    Designer.find(session[:designer_id])
+    session[:designer_id]? Designer.find(session[:designer_id]) : nil
   end
+
+  def logged_in?
+    current_user ? true : redirect_to(login_path)
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :logged_in?
+  before_action :logged_out?, only: :new
 
   def new
   end
@@ -17,5 +19,11 @@ class SessionsController < ApplicationController
   def destroy
     session.delete :designer_id
     redirect_to root_path
+  end
+
+  private
+
+  def logged_out?
+    current_user ? redirect_to(root_path) : true
   end
 end
