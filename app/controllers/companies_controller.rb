3ed37d529975e6739f3895bcf822558019e7b9fc
julientregoat <julienctregoat@gmail.com>
2018-01-23 14:26:29 -0500

    MULTI CHARTS WORKINGgit add .

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,17 +3,20 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
     @venues = @company.venues
-    data_table = GoogleVisualr::DataTable.new
-    data_table.new_column('string', 'Year' )
-    data_table.new_column('number', 'Fee')
+    @charts = {}
+    @venues.each do |ven|
+      data_table = GoogleVisualr::DataTable.new
+      data_table.new_column('string', 'Year' )
+      data_table.new_column('number', 'Fee')
 
-    sorted_contracts = @venues.first.contracts.sort_by{|contract| contract.opening_date}
-    sorted_contracts.each do |contract|
-      data_table.add_rows([[contract.opening_date.year.to_s, contract.fee]])
+      sorted_contracts = ven.contracts.sort_by{|contract| contract.opening_date}
+      sorted_contracts.each do |contract|
+        data_table.add_rows([[contract.opening_date.year.to_s, contract.fee]])
+      end
+
+      option = { width: 600, height: 240, title: 'Company Performance' }
+      @charts[ven.id] = GoogleVisualr::Interactive::AreaChart.new(data_table, option)
     end
-
-    option = { width: 600, height: 240, title: 'Company Performance' }
-    @chart = GoogleVisualr::Interactive::AreaChart.new(data_table, option)
   end
 
   private
