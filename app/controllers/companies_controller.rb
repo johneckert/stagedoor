cf9ea3a5f1eb4c5dbce8970860820cd0123cbbbb
julientregoat <julienctregoat@gmail.com>
2018-01-23 17:35:34 -0500

    working on grabbing all averages for better line graph

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,8 +3,8 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
     @venues = @company.venues
-
     @charts = {}
+
     @venues.each do |ven|
       data_table = GoogleVisualr::DataTable.new
       data_table.new_column('date', 'Year' )
@@ -14,27 +14,24 @@ class CompaniesController < ApplicationController
       data_table.new_column('number', 'Sound Design Fees')
       data_table.new_column('number', 'Projection Design Fees')
 
-      sorted_contracts = ven.contracts.sort_by{|contract| contract.opening_date}
-      sorted_contracts.each do |contract|
-        case contract.categories.first.id
-        when 1
-          data_table.add_rows([[contract.opening_date, contract.fee, nil, nil, nil, nil]])
-        when 2
-          data_table.add_rows([[contract.opening_date, nil, contract.fee, nil, nil, nil]])
-        when 3
-          data_table.add_rows([[contract.opening_date, nil, nil, contract.fee, nil, nil]])
-        when 4
-          data_table.add_rows([[contract.opening_date, nil, nil, nil, contract.fee, nil]])
-        when 5
-          data_table.add_rows([[contract.opening_date, nil, nil, nil, nil, contract.fee]])
-        else
-          data_table.add_rows([[contract.opening_date, nil, nil, nil, nil, nil]])
-        end
-      end
+      fees = {
+        Category.find(1) => {},
+        Category.find(2) => {},
+        Category.find(3) => {},
+        Category.find(4) => {},
+        Category.find(5) => {}
+      }
 
-      option = { width: 600, height: 240, title: 'Company Performance', trendlines: {
-        0 => {type: 'polynomial', degree: 3}
-        } }
+      ven.contracts.each do |contract|
+        year = contract.opening_date.year
+        category = contract.categories.first
+        fees[category][year] == nil ? fees[category][year] : true
+
+
+      data_table.add_rows([[contract.opening_date, contract.fee, nil, nil, nil, nil]])
+
+
+      option = { width: 600, height: 240, title: 'Company Performance'}
       @charts[ven.id] = GoogleVisualr::Interactive::ScatterChart.new(data_table, option)
     end
   end
