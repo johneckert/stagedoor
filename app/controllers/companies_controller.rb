d64f3c6639a9286f6ed9d8be9353db036eea0a2d
julientregoat <julienctregoat@gmail.com>
2018-01-23 18:14:40 -0500

    GRAPHS WORKING FOR AVGS

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -11,32 +11,54 @@ class CompaniesController < ApplicationController
 
     @venues.each do |ven|
       data_table = GoogleVisualr::DataTable.new
-      data_table.new_column('date', 'Year' )
+      data_table.new_column('string', 'Year' )
       data_table.new_column('number', 'Scenic Design Fees')
       data_table.new_column('number', 'Costume Design Fees')
       data_table.new_column('number', 'Lighting Design Fees')
       data_table.new_column('number', 'Sound Design Fees')
       data_table.new_column('number', 'Projection Design Fees')
 
+      scenic = Category.find(1)
+      costume = Category.find(2)
+      lighting = Category.find(3)
+      sound = Category.find(4)
+      projection = Category.find(5)
+
       fees = {
-        Category.find(1) => {},
-        Category.find(2) => {},
-        Category.find(3) => {},
-        Category.find(4) => {},
-        Category.find(5) => {}
+        scenic => {},
+        costume => {},
+        lighting => {},
+        sound => {},
+        projection => {}
       }
 
       ven.contracts.each do |contract|
-        year = contract.opening_date.year
+        year = contract.opening_date.year.to_s
         category = contract.categories.first
-        fees[category][year] == nil ? fees[category][year] : true
+        fees[category][year] == nil ? fees[category][year] = [] : true
+        fees[category][year] << contract.fee
+      end
 
+      start_year = ven.contracts.map{|con| con.opening_date.year}.min
+      end_year = ven.contracts.map{|con| con.opening_date.year}.max
+      i = start_year
 
-      data_table.add_rows([[contract.opening_date, contract.fee, nil, nil, nil, nil]])
+      while i <= end_year do
 
+        data_table.add_row(
+          [
+          i.to_s,
+          avg(fees[scenic][i.to_s]),
+          avg(fees[costume][i.to_s]),
+          avg(fees[lighting][i.to_s]),
+          avg(fees[sound][i.to_s]),
+          avg(fees[projection][i.to_s])
+        ])
+        i +=1
+      end
 
       option = { width: 600, height: 240, title: 'Company Performance'}
-      @charts[ven.id] = GoogleVisualr::Interactive::ScatterChart.new(data_table, option)
+      @charts[ven.id] = GoogleVisualr::Interactive::LineChart.new(data_table, option)
     end
   end
 
@@ -44,5 +66,13 @@ class CompaniesController < ApplicationController
 
   def company_params
     params.require(:company).permit(:name)
+  end
+
+  def avg(num_array)
+    if num_array == nil
+      return 0
+    else
+      num_array.inject(:+) / num_array.count
+    end
   end
 end
