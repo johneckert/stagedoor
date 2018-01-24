128b13681914a0db24bda3c929b50571ab2cbe29
julientregoat <julienctregoat@gmail.com>
2018-01-23 22:46:32 -0500

    fixed visuals to account for no data

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -55,7 +55,7 @@ class CompaniesController < ApplicationController
           avg(fees[projection][i.to_s])
         ])
         i +=1
-      end
+      end unless i == nil
 
       option = { width: 600, height: 240, title: 'Company Performance'}
       @charts[ven.id] = GoogleVisualr::Interactive::LineChart.new(data_table, option)
